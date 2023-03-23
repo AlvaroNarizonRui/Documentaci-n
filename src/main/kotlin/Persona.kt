02b2384673973c8595fd81e8ec5047a3713898ee
Diego <aruigal2303@g.educaand.es>
2023-03-23 17:28:11 +0100

    se hizo la documentación minimo de la clase persona

--- src/main/kotlin/Persona.kt
+++ src/main/kotlin/Persona.kt
@@ -1,3 +1,13 @@
+/**
+ * FUNCIÓN DE LA PERSONA:
+ * @property Persona servirá como plantilla
+ * para todos los miembros del gimnasio que creemos.
+ *
+ *
+ *  ATRIBUTOS DE CLASE:
+ *  @param nombre será el nombre de la persona
+ *  @param edad será la edad de la persona
+ */
 abstract class Persona(nombre:String,edad:Int) {
     var nombre=nombre
         get(){
@@ -15,9 +25,11 @@ abstract class Persona(nombre:String,edad:Int) {
                 field=value
             }else println("No tienes la edad para entrar al gimnasio")
         }
-
+    //Es una función abstracta que dependiendo que persona calculará el balance
+    //mensual (lo que tenga que pagar cada persona o en caso del trabajador lo que cobrará.
     abstract fun balanceMensual():Int
 
+    //En esta función se mostrará los datos de cada persona
     override fun toString(): String {
         return("Datos: \n " +
                 "nombre: $nombre \n" +
